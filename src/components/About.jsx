8e213fa1ec8573687a49285652b1c1bd3078bcc6
kamser <keylor.morataya@gmail.com>
2026-05-26 23:28:56 -0700

    WRONG WAY - JUST FOR TEST - Modified the about and home components adding to them a button where, on the onClick, they use the navigate function. This fix the issue that loads all the dependencies on rounting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
+import { navigate } from "../routing/utils";
+
 export function AboutPage(){
     return (
         <>
             <h1>About Section</h1>
             <p>This is the about page section to test the app rounting</p>
             <a href="/">Home</a>
+            <button onClick={() => navigate('/')}>Go Home</button>
         </>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
+import { navigate } from "../routing/utils";
+
 export function HomePage(){
     return (
         <>
             <h1>Home</h1>
             <p>This is the home page</p>
             <a href="/about">Go to about</a>
+            <button onClick={() => navigate('/about')}>Go to About</button>
         </>
     )
 }
