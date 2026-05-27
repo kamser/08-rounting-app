ab6f7505ef9806fdf83fa651edbda3e155b696ff
kamser <keylor.morataya@gmail.com>
2026-05-27 01:02:57 -0700

    Create a constant to improve the readablity for the primary button event for the filters on the custom link

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,9 +1,10 @@
+import { BUTTON_EVENT } from "../routing/constants";
 import { navigate } from "../routing/utils";
 
 export function CustomLink({target, to, ...props}){
     const handleOnClick = (event) => {
 
-        const isMainEvent = event.button === 0
+        const isMainEvent = event.button === BUTTON_EVENT.PRIMARY_BUTTON // Zero is the main button. For some could be the right, but for other could be the left
         const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shiftKey
         const isManageableEvent = target === undefined || target === '_self'
 
--- src/routing/constants.js
+++ src/routing/constants.js
@@ -2,3 +2,7 @@ export const NAVIGATION_EVENT = {
     PUSH_STATE: 'pushstate', 
     POP_STATE: 'popstate'
 }
+
+export const BUTTON_EVENT = {
+    PRIMARY_BUTTON: 0
+}
