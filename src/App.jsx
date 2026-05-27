a7dd87f9fc9c3f20c394b51297f94614a6c5f222
kamser <keylor.morataya@gmail.com>
2026-05-27 01:17:43 -0700

    Imported and implemented the Router created before on the App component and deleted all the unnecessary code from the refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { AboutPage } from './components/About'
-import { HomePage } from './components/Home'
-
-import { NAVIGATION_EVENT } from './routing/constants.js'
+import { AppRouter } from './routing/AppRouter.jsx'
+import { appRoutes } from './routing/routes.js'
 
 function App() {
-  
-  const [currentPage, setCurrentPage] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPage(window.location.pathname)
-    }
-
-    window.addEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
-    window.addEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
-
-    return () => {
-      window.removeEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
-      window.removeEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
-    }
-  })
 
   return (
     <main>
-      {currentPage === '/' && <HomePage/>}
-      {currentPage === '/about' && <AboutPage/>}
+      <AppRouter routes={appRoutes}/>
     </main>
   )
 }
