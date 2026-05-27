935a7a98cc15c3f9c39633c9020f050fa0fd4c08
kamser <keylor.morataya@gmail.com>
2026-05-26 18:33:10 -0700

    Created two basic components for the app

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export function AboutPage(){
+    return (
+        <>
+            <h1>About Section</h1>
+            <p>This is the about page section to test the app rounting</p>
+            <a href="/">Home</a>
+        </>
+    )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export function HomePage(){
+    return (
+        <>
+            <h1>Home</h1>
+            <p>This is the home page</p>
+            <a href="/about">Go to about</a>
+        </>
+    )
+}
