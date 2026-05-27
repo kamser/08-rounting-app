6413cfc96a16ed8fc8eacc885ca3953a734be145
kamser <keylor.morataya@gmail.com>
2026-05-26 23:09:21 -0700

    Created a navigation funtion that creates a navigation event to navigate to an specific page

---
+++ src/routing/utils.js
@@ -0,0 +1,7 @@
+const NAVIGATION_EVENT = 'pushstate'
+
+export function navigate(nextPageRef){
+    window.history.pushState({}, '', nextPageRef)
+    const navigationEvent = new Event(NAVIGATION_EVENT)
+    window.dispatchEvent(navigationEvent)
+}
