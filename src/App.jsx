cab562a2274c64b41763d5734001540f81c39999
kamser <keylor.morataya@gmail.com>
2026-05-26 23:01:01 -0700

    First basic approach for routing using useState, but still with the issue that load all resource on routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import { AboutPage } from './components/About'
 import { HomePage } from './components/Home'
 
 function App() {
+  
+  const [currentPage, setCurrentPage] = useState(window.location.pathname)
 
   return (
-    <>
-      <HomePage/>
-      <AboutPage/>
-    </>
+    <main>
+      {currentPage === '/' && <HomePage/>}
+      {currentPage === '/about' && <AboutPage/>}
+    </main>
   )
 }
 
