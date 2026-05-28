624d8cf828b13f89735ffe731a592fcdc7bd880e
kamser <keylor.morataya@gmail.com>
2026-05-28 15:06:36 -0700

    Added the children value as props to access to it and obtain the data stored in it and apply format to the code

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -2,38 +2,39 @@ import { useState, useEffect } from "react";
 import { NAVIGATION_EVENT } from "./constants";
 import { match } from 'path-to-regexp'
 
-export function AppRouter({routes = [], defaultComponent: DefaultComponent = () => <h1>404 Error</h1>}){
+export function AppRouter({children, routes = [], defaultComponent: DefaultComponent = () => <h1>404 Error</h1>}){
     
     const [currentPath, setCurrentPath] = useState(window.location.pathname)
     
-      useEffect(() => {
-        const onLocationChange = () => {
-          setCurrentPath(window.location.pathname)
-        }
+    useEffect(() => {
+      const onLocationChange = () => {
+        setCurrentPath(window.location.pathname)
+      }
+  
+      window.addEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
+      window.addEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
+  
+      return () => {
+        window.removeEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
+        window.removeEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
+      }
+    }, [])
+
+    let routeParams = {}
+
+    const Page = routes.find(({path}) => {
+      if(path === currentPath) return true
+
+      const matcherUrl = match(path, {decode: decodeURIComponent})
+      const matched = matcherUrl(currentPath)
+      if(!matched) return false
+
+      routeParams = matched.params
+      return true
     
-        window.addEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
-        window.addEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
+    })?.Component
     
-        return () => {
-          window.removeEventListener(NAVIGATION_EVENT.PUSH_STATE, onLocationChange)
-          window.removeEventListener(NAVIGATION_EVENT.POP_STATE, onLocationChange)
-        }
-      }, [])
-
-      let routeParams = {}
-
-      const Page = routes.find(({path}) => {
-        if(path === currentPath) return true
-
-        const matcherUrl = match(path, {decode: decodeURIComponent})
-        const matched = matcherUrl(currentPath)
-        if(!matched) return false
-
-        routeParams = matched.params
-        return true
-      
-      })?.Component
-      return Page 
-                ? <Page routeParams={routeParams}/> 
-                : <DefaultComponent routeParams={routeParams}/>
+    return Page 
+              ? <Page routeParams={routeParams}/> 
+              : <DefaultComponent routeParams={routeParams}/>
 }
