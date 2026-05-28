e22e44ae512480a985d4962f18dc7f8d0c28efa7
kamser <keylor.morataya@gmail.com>
2026-05-28 14:47:04 -0700

    Add the imports for the page and the new route component, and for each page, add a route component in the children field of the AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { AppRouter } from './routing/AppRouter.jsx'
+import { Route } from './components/Route.jsx'
 import { appRoutes } from './routing/routes.js'
+
 import { NotFoundPage } from './pages/NotFound.jsx'
+import { HomePage } from './pages/Home.jsx'
+import { AboutPage } from './pages/About.jsx'
 
 function App() {
 
   return (
     <main>
-      <AppRouter routes={appRoutes} defaultComponent={NotFoundPage}/>
+      <AppRouter routes={appRoutes} defaultComponent={NotFoundPage}>
+        <Route path='/' Component={HomePage}/>
+        <Route path='/' Component={AboutPage}/>
+      </AppRouter>
     </main>
   )
 }
