e0c091b3188bcd1986581653e7d705ff85d5911a
kamser <keylor.morataya@gmail.com>
2026-05-28 22:51:53 -0700

    Added the second test and a beforEach funtion to clean the cases for every test

--- src/routing/AppRouter.test.jsx
+++ src/routing/AppRouter.test.jsx
@@ -1,9 +1,20 @@
-import {describe, it, expect} from 'vitest'
-import {render} from '@testing-library/react'
+import {describe, it, expect, beforeEach} from 'vitest'
+import {render, screen, cleanup} from '@testing-library/react'
 import { AppRouter } from './AppRouter'
 
 describe('Router', () => {
+
+    beforeEach(() => {
+        cleanup()
+    })
+
     it('should render without problems', () => {
         render(<AppRouter routes={[]}/>)
+        expect(true).toBeTruthy()
+    })
+
+    it('should render 404 if no routes mathc', () => {
+        render(<AppRouter routes={[]} defaultComponent={() => <h1>404</h1>}/>)
+        expect(screen.getByText('404')).toBeTruthy()
     })
 })
