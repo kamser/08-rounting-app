6e249555f3b52dd532eba3d86c62be69f3262791
kamser <keylor.morataya@gmail.com>
2026-05-28 21:24:03 -0700

    Added the internacionalisation to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,29 @@
 import { CustomLink } from '../components/CustomLink.jsx';
 
-export default function AboutPage(){
+const i18n = {
+    es:{
+        title: 'Sobre nosotros',
+        description: 'Hola, soy keylor y esta es la pagina de sobre nosotros',
+        buttonText: 'Volver a Inicio'
+    },
+    en:{
+        title: 'About us',
+        description: 'Hello, Im keylor and this is a page about us',
+        buttonText: 'Back Home'
+    }
+}
+
+const useI18n = (lang) => {
+    return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({routeParams}){
+    const i18n = useI18n(routeParams.lang ?? 'es')
     return (
         <>
-            <h1>About Section</h1>
-            <p>This is the about page section to test the app rounting</p>
-            <CustomLink to={'/'}>Go Home</CustomLink>
+            <h1>{i18n.title}</h1>
+            <p>{i18n.description}</p>
+            <CustomLink to={'/'}>{i18n.buttonText}</CustomLink>
         </>
     )
 }
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,6 +1,11 @@
+import AboutPage from '../pages/About.jsx';
 import { SearchPage } from '../pages/Search.jsx';
 
 export const appRoutes = [
+    {
+        path:'/:lang/about',
+        Component: AboutPage
+    },
     {
         path:'/search/:query',
         Component: SearchPage
