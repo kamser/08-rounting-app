2901700c29b96f94471101f7ed6c21919febf34d
kamser <keylor.morataya@gmail.com>
2026-05-28 21:40:04 -0700

    Create the test to check that the router component render without any issues

--- src/routing/AppRouter.test.jsx
+++ src/routing/AppRouter.test.jsx
@@ -1,7 +1,9 @@
 import {describe, it, expect} from 'vitest'
+import {render} from '@testing-library/react'
+import { AppRouter } from './AppRouter'
 
 describe('Router', () => {
-    it('should work', () => {
-        expect(1).toBe(1)
+    it('should render without problems', () => {
+        render(<AppRouter routes={[]}/>)
     })
 })
